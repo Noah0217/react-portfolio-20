31b17f20521d45dead6f8bf82583dc8b8e6e62f0
Noah <noahm0217@gmail.com>
2021-10-27 20:28:26 -0600

    starter code/header complete

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section>
+    <LeftSection>
+      <SectionTitle>
+Welcome To <br />
+My Portfolio
+      </SectionTitle>
+      <SectionText>
+       
+      </SectionText>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
