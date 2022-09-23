f69baed5d4dd2766599c9f31ce68cab5edd958c4
Noah <noahm0217@gmail.com>
2022-09-22 23:45:22 -0600

    fixed image sizing/changed project images and added Netlflix clone to projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
 export const projects = [
   {
     title: 'Game Stork',
-    description: "An app built using JavaScript, HTML5, CSS3, and Rawg's video game API. This application allows the user to to find new vide games based on there likings and difficulty preferences. This was our first group collaboration project as a team, in my coding bootcamp experience. ",
-      image: '/images/videogamemusic.jpeg',
+    description: "Game Stork an application built using JavaScript, HTML5, CSS3, and Rawg's video game API. This application allows the user to to find new vide games based on there likings and difficulty preferences. This was our first group collaboration project as a team, in my coding bootcamp experience. ",
+      image: 'https://user-images.githubusercontent.com/84366215/191897395-0077ba46-164e-41ac-a2ef-25dd858d860f.jpg',
       tags: ['JavaScript', 'HTML5', 'CSS3'],
     source: 'https://github.com/ewalker518/game-go',
     visit: 'https://ewalker518.github.io/game-stork/',
@@ -10,8 +10,8 @@ export const projects = [
   },
   {
     title: 'Workout Tracker',
-    description:"A Workout tracker app built using Node.js, Sequelize, MySQL and Express. Project required modules for Sequelize, routes via Express, MySQL Shell to start schemas and seeds for the MySQL database. This was our second group collaboration project as a team, in my coding bootcamp experience.",
-    image: '/images/myfitnesspal.jpg',
+    description: "An app built using React, Sequelize ORM, Node.js, and Express.js. Workout Tracker is an application that keeps track and creates goals for the user to accomplish their goals throughout their workout journey, users can save progress and customize their own routine within the calendar. ",
+    image: 'https://user-images.githubusercontent.com/84366215/191896974-cfa3a744-6b53-4be5-8c07-83c6aeab0ccb.jpg',
     tags: ['Node', 'Express', 'JavaScript'],
     source: 'https://github.com/tyler1836/workout-tracker',
     visit: 'https://blooming-shelf-02541.herokuapp.com/',
@@ -19,7 +19,7 @@ export const projects = [
   },
   {
     title: 'Clean Freak',
-    description: "Clean Freak, an app built using MongoDB, Express, React and Node.js. Clean Freak is an application that can be used to schedule cleaning appointments for your needs.",
+    description: "Clean Freak is an application that allows the user to view, and purchase different cleaning packages for their home, once they've picked their desired package they can completely go through the order process entirely online.",
       image: '/images/cleaning-image.jpg',
       tags: ['MongoDB', 'Express', 'React', 'Node'],
     source: 'https://github.com/brandon-stewart-rgb/clean-freak/tree/main',
@@ -27,12 +27,12 @@ export const projects = [
     id: 2,
   },
   {
-    title: 'Note Taker',
-    description: "A back-end server side note taker app built with Node.Js, Express, and JavaScript that allows the user to save and update notes as they need through out there day.",
-    image: '/images/pen-paper.jpg',
-    tags: ['Node', 'Express', 'JavaScript'],
-    source: 'https://github.com/Noah0217/note-taker-11',
-    visit: 'https://note-taker-noah.herokuapp.com/',
+    title: 'Netflix Clone',
+    description: "A full functioning Netflix clone built using React, Tailwind CSS, and back end built using Google Firebase and TMDB Rest API. The User can sign in and out and save their favorite movies and manage them within the account page.",
+    image: 'https://wwwflickeringmythc3c8f7.zapwp.com/q:i/r:1/wp:1/w:362/u:https://cdn.flickeringmyth.com/wp-content/uploads/2019/08/Netflix-logo-600x400.jpg',
+    tags: ['React', 'Tailwind CSS', 'Firebase'],
+    source: 'https://github.com/Noah0217/netflix-clone',
+    visit: 'https://netflix-clone-react-1d028.firebaseapp.com/',
     id: 3,
   },
 ];
